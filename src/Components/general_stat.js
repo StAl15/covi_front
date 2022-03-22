3e0ed72326d177ebb14e34b0f168562e617a83c5
StAl15 <stihinaleksander@yandex.ru>
2022-03-22 16:13:36 +0300

    minor changes

--- src/Components/general_stat.js
+++ src/Components/general_stat.js
@@ -4,8 +4,8 @@ import "../Styles/genstat.css";
 
 export const CustomGenStat = () => {
     return (
-        <BDiv shadow="sm" p="4" bg="white" style={{ borderRadius: '20px', borderColor: 'white', width:'fit-content' }}>
-            <h5>Ваше состояние</h5>
+        <BDiv shadow="sm" p="4" bg="white" style={{ borderRadius: '20px', borderColor: 'white', width:'fit-content', marginBottom:'20px' }}>
+            <h5>Ваше состояние:</h5>
             <h4 className="curent_patient_state" style={{color:'#4A74B0'}}>НЕУДОВЛЕТВОРИТЕЛЬНО</h4>
             <button>Звонок</button>
 
