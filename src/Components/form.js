d01cdf22d7ba1df9a5d3fb93eb7c46e5ef1f16f4
alex <stihinaleksander@yandex.ru>
2022-05-05 16:31:34 +0500

    upd width input

--- src/Components/form.js
+++ src/Components/form.js
@@ -6,7 +6,7 @@ export default function CustomFormInput({ title, hint, input_type, onChange }) {
     return (
         <Form.Group style={{marginBottom:'30px'}}>
             <label htmlFor="input_form">{title}</label>
-            <Form.Input style={{ width: 'fit-content', borderColor:'white', borderRadius:'10px', paddingTop:'25px', paddingBottom:'25px' }} type={input_type} id="input_form" classname="input_form" placeholder={hint} shadow='sm'/>
+            <Form.Input style={{ width: '100%', borderColor:'white', borderRadius:'10px', paddingTop:'25px', paddingBottom:'25px' }} type={input_type} id="input_form" classname="input_form" placeholder={hint} shadow='sm'/>
         </Form.Group>
     )
 }
